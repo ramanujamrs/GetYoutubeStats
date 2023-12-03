7a13dcbd9ba89a256878875a5fe8e13bac9f8180
Ramanujam Srinivasan <ramanujam.rs@gmail.com>
2023-12-02 22:21:55 -0600

    Added insert statement for saving video view count in DB

--- YouTubeStatistics.py
+++ YouTubeStatistics.py
@@ -1,7 +1,13 @@
+# Author: Ramanujam Srinivasan
+# Get Youtube video statistics - viewcount for each video and save it in DB
+
 import os
+import requests
 
 from mysql import connector
 from dotenv import load_dotenv
+
+from datetime import datetime
 
 # Access environment variables
 load_dotenv()
@@ -9,6 +15,11 @@ HOST = os.getenv("HOST")
 USERNAME = "ramanujam"
 PASSWORD = os.getenv("PASSWORD")
 DB = os.getenv("DB")
+KEY = os.getenv("KEY")
+
+# INSERT Statement
+single_record = "INSERT INTO yt_video_viewcount (video_id, view_count,datetime)\
+    VALUES (%s, %s, %s)"
 
 # Connect to DB
 # Get Video Ids from table
@@ -25,7 +36,28 @@ try:
             cursor.execute(select_specific_cols)
             result = cursor.fetchall()
             for row in result:
-                print(row)
+                print(row[0])
+                videoId = row[0]
+                videostats = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id="+ videoId +"&key="+KEY
+                statsrequest = requests.get(videostats)
+                statsvideos = statsrequest.json()
+
+                for st in statsvideos["items"]: 
+                    #print(st["statistics"]["viewCount"])
+                    now = datetime.now()
+                    print("now =", now)
+                    # dd/mm/YY H:M:S
+                    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+                    video_records = [
+                    (
+                        videoId,
+                        st["statistics"]["viewCount"],
+                        dt_string
+                    )]
+                with database.cursor() as cursor:
+                    cursor.executemany(single_record, video_records)
+                    database.commit()
+        database.close()
 except connector.Error as e: 
     print(e)
 
